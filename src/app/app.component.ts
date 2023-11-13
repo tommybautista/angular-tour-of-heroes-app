dd64decd54321cbe95bf7c65bc5672568f2764a6
Tommy Bautista <tommybautista320@gmail.com>
2023-11-13 13:23:57 -0500

    Lesson 1 complete

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,5 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'angular-tour-of-heroes-app';
+  title = 'Tour of Heroes';
 }
